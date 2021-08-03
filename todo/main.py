72c58b19ba730c5579fb67ef664eee110560000a
Joshua Goss <josh@joshgoss.com>
2021-08-02 21:18:34 -0400

    add expiration to token endpoint response

--- todo/main.py
+++ todo/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import SessionLocal, engine
 from .routers import todos, token, users, healthcheck
 from .schemas import Message
+from .config import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(todos.router)
