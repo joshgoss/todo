a9ecac7b502270e8a48905784c40caa2c84d9691
Joshua Goss <josh@joshgoss.com>
2023-10-20 09:58:48 -0400

    use secrets to get from parameter store within task definition

--- backend/todo/config.py
+++ backend/todo/config.py
@@ -2,15 +2,15 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    access_token_expire_minutes: str
+    access_token_expire_minutes: int
     access_token_algorithm: str
     app_name: str
     app_host: str
-    app_port: str
+    app_port: int
     postgres_db: str
     postgres_host: str
     postgres_password: str
-    postgres_port: str
+    postgres_port: int
     postgres_user: str
     secret_key: str
 
@@ -19,8 +19,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
-print(f"POSTGRES PORT: {settings.postgres_port}")
-print(f"POSTGRES USER: {settings.postgres_user}")
-print(f"POSTGRES HOST: {settings.postgres_host}")
-print(f"APP_PORT: {settings.app_port}")
